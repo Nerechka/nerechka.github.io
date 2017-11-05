3fea64300aed158d875bdc06328ece86459213aa
Nerechka <sill@mail.ru>
2017-11-05 22:19:18 +0300

    Add js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,4 @@
-
 alert('А ты уже узнал код доступа у хозяйки сайта?')
 let year = prompt('Prove it!', '');
 if (year === null) window.close();
-if (year != 'It`s sunflower!') alert( 'See you soon! ;)'
+if (year != 'It`s sunflower!') alert( 'Maybe next time..)' ), window.close();
