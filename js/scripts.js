a1fca5fd7f842f27f2ecd61cf5ebe210c6c89ded
Nerechka <32274153+Nerechka@users.noreply.github.com>
2017-11-05 22:02:46 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,5 @@
 
+alert('А ты уже узнал код доступа у хозяйки сайта?')
+let year = prompt('Prove it!', '');
+if (year === null) window.close();
+if (year != 'It`s sunflower!') alert( 'See you soon! ;)'
