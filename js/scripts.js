bcac023eb3791a44186efaa71c067b190fcda465
Nerechka <sill@mail.ru>
2017-11-05 22:59:45 +0300

    Add fix again (about js)

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
 alert('А ты уже узнал код доступа у хозяйки сайта?')
 let year = prompt('Prove it!', '');
 if (year === null) window.close();
-if (year != 'It`s sunflower!') alert( 'Maybe next time..)' ), window.close();
+if (year != 'It`s sunflower!') alert( 'Maybe next time..)'), document.getElementsByTagName('div').className += ' BYE';
